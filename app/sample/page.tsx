cb732eda9bdb08961316a91cfb5c51b31592f158
dianaamriyeva-ux <diana.amriyeva@gmail.com>
2026-02-18 19:37:22 +0000

    Oops, now message here ... CK

---
+++ app/sample/page.tsx
@@ -0,0 +1,15 @@
+import { AccordionBasic } from "./accordion"
+import { CardImage } from "./card"
+
+export default function Page() {
+    return <>
+
+        <div className="mt-10 flex flex-col items-center gap-5">
+
+            <CardImage />
+            <AccordionBasic />
+
+        </div>
+
+    </>
+}
